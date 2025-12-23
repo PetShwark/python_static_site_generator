0492a315024f5aff125d701222b4b3759f92cc11
Peter Schwenk <paschwenk@gmail.com>
2025-12-22 22:22:00 -0500

    After "TextNode Tests" lesson

---
+++ src/test_textnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    def test_noteq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node with different text", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    def test_eq2(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(node, node2)
+    def test_noteq2(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+    def test_noteq3(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a image node", TextType.IMAGE, "image.png")
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
