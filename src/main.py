9edd4b393b89a096b7f3fc6f9de98deb4eead3d8
Peter Schwenk <paschwenk@gmail.com>
2025-12-29 14:23:13 -0500

    After "Copy Static" lesson

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+import logging
+logging.basicConfig(level=logging.INFO)
+
+def copy_folder(src: str, dest: str) -> None:
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+    else:
+        logging.info(f"Destination folder {dest} already exists. Erasing contents.")
+        for item in os.listdir(dest):
+            item_path = os.path.join(dest, item)
+            if os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+            else:
+                os.remove(item_path)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dest, item)
+        if os.path.isdir(s):
+            copy_folder(s, d)
+        else:
+            logging.info(f"Copying {s} to {d}")
+            shutil.copy(s, d)
 
 def main():
-    tn = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(tn)
+    copy_folder("static", "public")
 
 if __name__ == "__main__":
     main()
